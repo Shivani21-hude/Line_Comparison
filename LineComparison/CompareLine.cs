71f7e5de5ff6c1871c560fee7e6d5d1b96a1696b
Shivani21-hude <shivani21hude@gmail.com>
2023-01-05 19:01:02 +0530

    [Shivani]-Calculate length of line

---
+++ LineComparison/CompareLine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class CompareLine
+    {
+        public static void Compare()
+        {
+
+            Console.WriteLine("enter values for line: ");
+            Console.Write("x1: ");
+            double x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("x2: ");
+            double x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y1: ");
+            double y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y2: ");
+            double y2 = Convert.ToInt32(Console.ReadLine());
+
+            double length = Math.Sqrt((Math.Pow((x2 - x1), 2)) + (Math.Pow((y2 - y1), 2)));
+            Console.WriteLine("The lenth of the given cordinate is: " + length);
+        }
+    }
+}
---
+++ LineComparison/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LineComparison
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Line Comparison Problem \n ");
+            CompareLine.Compare();
+        }
+    }
+}
